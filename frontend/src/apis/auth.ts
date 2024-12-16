9974dd1abf287abbd50e446ac48df0fd264913dd
ZeoXer <andrew17413@gmail.com>
2024-12-16 18:49:31 +0800

    feat: add signup, login api function

--- frontend/src/apis/auth.ts
+++ frontend/src/apis/auth.ts
@@ -1,8 +1,13 @@
 import { Axios } from "./axios";
 
-export const signup = async (email: string, password: string) => {
+export const signup = async (
+  userName: string,
+  email: string,
+  password: string
+) => {
   try {
-    const response = await Axios.post("/signup", {
+    const response = await Axios.post("/api/auth/signup", {
+      userName,
       email,
       password,
     });
@@ -10,6 +15,21 @@ export const signup = async (email: string, password: string) => {
     return response.data;
   } catch (error) {
     console.error("Error signing up:", error);
+
+    return null;
+  }
+};
+
+export const login = async (email: string, password: string) => {
+  try {
+    const response = await Axios.post("/api/auth/login", {
+      email,
+      password,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error logging in:", error);
 
     return null;
   }
