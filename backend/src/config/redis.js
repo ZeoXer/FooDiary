b7ca8d1237487519001b218e5be32515b383d350
ching11720 <jasmine910402614@gmail.com>
2024-12-21 22:23:55 +0800

    fix&build: fix redis connection and build docker-compose

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -2,8 +2,6 @@ const redis = require('redis');
 require('dotenv').config();
 
 const redisClient = redis.createClient({
-    username: 'default',
-    password: process.env.REDIS_PASS,
     socket: {
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT
