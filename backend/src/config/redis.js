bcd8a8beaa96c9d2a0a1c750a1a31ffbe6e57b7a
ching11720 <jasmine910402614@gmail.com>
2024-12-30 17:14:21 +0800

    build:docker compose and add .env.exmaple and adjust redis connection

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 
 const redisClient = redis.createClient({
     username: 'default',
-    password: process.env.REDIS_PASS,
     socket: {
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT
